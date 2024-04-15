8072a88aee3bfae993a2217b441d3e72ea74e860
Daniyor <dani.bolta87@gmail.com>
2024-04-15 23:06:49 +0800

    add social login icons

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import { FcGoogle } from 'react-icons/fc'
-import { FaGithub } from 'react-icons/fa'
+import React from "react";
+import { Button } from "../ui/button";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
-export const Social=()=> {
+export const Social = () => {
   return (
-    <div className='flex items-center w-full gap-x-2'>
-        <Button size='lg' variant='outline' className='w-full'>
-            <FcGoogle className='h-5 w-5'/>
-        </Button>
-        <Button size='lg' variant='outline' className='w-full'>
-            <FaGithub className='h-5 w-5'/>
-        </Button>
+    <div className="flex items-center w-full gap-x-2">
+      <Button size="lg" variant="outline" className="w-full">
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+      <Button size="lg" variant="outline" className="w-full">
+        <FaGithub className="h-5 w-5" />
+      </Button>
     </div>
-  )
-}
+  );
+};
