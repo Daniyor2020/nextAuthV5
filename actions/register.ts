994e6578422775ef8b2a43cec1608afe64221f83
Daniyor <daniyor.b@userdata.tech>
2024-06-06 00:19:38 +0800

    store user in db

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,10 @@
 
 import { z } from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+
+
 
 export const register =async (values:z.infer<typeof RegisterSchema>)=> {
 
@@ -15,13 +19,43 @@ export const register =async (values:z.infer<typeof RegisterSchema>)=> {
             }
         };
     }
+
+    const { email, password, name } = validatedFields.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where:{
+            //@ts-ignore
+            email
+        
+        }
+    })
+
+    if(existingUser){
+        return {
+            success: false,
+            message: 'User already exists',
+            data: {
+                errors: ['User already exists']
+            }
+        };
+    }
+
+  await db.user.create({
+        data:{
+            //@ts-ignore
+            email,
+            name,
+            password: hashedPassword
+        }
+    });
+
+    // TODO: Send email verification
     return {
         success: true,
-        message: 'Register successfullly!',
-        data: {
-            userName: 'John Doe',
-            userId: 1,
-        }
+        message: 'User created successfully',
+       
     };
     
 };
